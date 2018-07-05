2b2820f03e0a7c0b42befac6476b003af957e122
yuanmin21 <yuanmin@marvell.com>
2018-07-05 18:24:32 +0800

    test node

--- test.py
+++ test.py
@@ -4,12 +4,15 @@ a = 10
 b = 20
 
 if __name__ == "__main__":
+	
 	print ("The simple calculator")
 	print ("a = ", a)
 	print ("b = ", b)
 	print("-------------")
 	print ("a + b", a + b)
-	c=os.system("pwd")
+	os.system("pwd")
+	
+	os.system("ssh $root@10.25.132.123")
 	print(c)
 	print("-------------")
 
